d7b7ce0a751bc8f2f4f667eab167580eba0bc003
Christoph Sauter <christophdsauter@gmail.com>
2019-02-20 15:07:28 -0600

    sticky collision attempt

---
+++ Assets/sticky_collide.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sticky_collide : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "sticky")
+        {
+            rb.velocity = new Vector2(0, 0);
+            Debug.Log("Sticky Collision registered.");
+        }
+    }
+}
