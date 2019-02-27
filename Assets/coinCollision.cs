696ab6f86829a313b1aa5fb5237751013b9a42d3
csauterwhiteout <csauter@whiteoutsolutions.com>
2019-02-27 15:10:48 -0600

    sticky wallas a coins

---
+++ Assets/coinCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.GetComponent<LaunchSprite>());
+        if (other.gameObject.GetComponent<LaunchSprite>())
+        {
+            gameObject.SetActive(false);
+            //var scoreScript = other.gameObject.GetComponent("Score Counter");
+            //scoreScript.score += 1;
+            Debug.Log("coin collision registered");
+        }
+    }
+}
---
+++ Assets/scoreCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreCounter : MonoBehaviour
+{
+    public int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
